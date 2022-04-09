ef2f7f7c0478776ec5ac742427bafe8f62b69739
UbaidUllah <79785631+Ubaidullah13@users.noreply.github.com>
2022-04-09 13:20:17 +0500

    Led Blink Esp32

---
+++ ESP 32 Led blink test and code/Inner Led Blink Code.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+#define led 2
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for (int i = 0; i < 5; i++)
+  {
+      digitalWrite(led ,HIGH);
+
+      delay(500);
+
+      digitalWrite(led, LOW);
+
+      delay(500);
+  }
+  
+  for (int i = 0; i < 5; i++)
+  {
+      digitalWrite(led ,HIGH);
+
+      delay(1000);
+
+      digitalWrite(led, LOW);
+
+      delay(1000);
+  }
+
+}
