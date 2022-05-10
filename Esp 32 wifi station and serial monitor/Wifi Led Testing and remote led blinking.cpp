2e9abaa5e9de9a6957765f98c02ace367e84e0b7
UbaidUllah <79785631+Ubaidullah13@users.noreply.github.com>
2022-05-10 10:31:58 +0500

    Update Wifi Led Testing and remote led blinking.cpp

--- Esp 32 wifi station and serial monitor/Wifi Led Testing and remote led blinking.cpp
+++ Esp 32 wifi station and serial monitor/Wifi Led Testing and remote led blinking.cpp
@@ -1,16 +1,3 @@
-/*
-  WiFiAccessPoint.ino creates a WiFi access point and provides a web server on it.
- 
-  Steps:
-  1. Connect to the access point "yourAp"
-  2. Point your web browser to http://192.168.4.1/H to turn the LED on or http://192.168.4.1/L to turn it off
-     OR
-     Run raw TCP "GET /H" and "GET /L" on PuTTY terminal with 192.168.4.1 as IP address and 80 as port
- 
-  Created for arduino-esp32 on 04 July, 2018
-  by Elochukwu Ifediora (fedy0)
-*/
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
@@ -43,7 +30,7 @@ void setup()
 
 void loop()
 {
-  WiFiClient client = server.available(); // listen for incoming clients
+  WiFiClient client = server.available(); // incoming clients
 
   if (client)
   {                                // if you get a client,
