8ccb94f46e2968dbc87749ebabb945131d39aeed
Ubaidullah <ubaid.99es@gmail.com>
2022-05-15 18:18:50 +0500

    Lock

--- Car Security System/src/main.cpp
+++ Car Security System/src/main.cpp
@@ -27,41 +27,53 @@
 #define USER_EMAIL "ubaid.99es@gmail.com"
 #define USER_PASSWORD "TestEsp32"
 
-// variables
-#define Lock 22
-#define Door 21
-#define Relay 19
-#define engine 23
+// PINS
+#define LOCK 23
+#define DOOR 22
+#define ENGINE 21
+#define RELAY 19
 
-int LockSignal = 0;
-int DoorSignal = 0;
-int EngineSignal = 0;
+// Signal Variable
+int lockSignal = 0;
+int doorSignal = 0;
+int engineSignal = 0;
 
-// Define Firebase Data object
+// Firebase Data object
 FirebaseData fbdo;
 
 FirebaseAuth auth;
 FirebaseConfig config;
-int read_data;
-unsigned long sendDataPrevMillis = 0;
-unsigned long count = 0;
-int intValue;
 
-int engineCount = 0;
-int doorCount = 0;
-int LockCount = 0;
+int read_data = 0;
+int n = 0;
+
+unsigned long sendDataPrevMillis = 0;
+// unsigned long count = 0;
+// int intValue;
+
+// int engineCount = 0;
+// int doorCount = 0;
+// int LockCount = 0;
 void setup()
 {
 
   Serial.begin(115200);
-  pinMode(Lock, INPUT);
-  pinMode(Door, INPUT);
-  pinMode(Relay, OUTPUT);
-  pinMode(engine, INPUT);
-  pinMode(2, OUTPUT);
 
+  // Input pins -> Signal form Component towrds ESP
+  pinMode(LOCK, INPUT);
+  pinMode(DOOR, INPUT);
+  pinMode(ENGINE, INPUT);
+
+  // Output Pins -> Signal from ESP towards Component
+  pinMode(RELAY, OUTPUT);
+
+  //  pinMode(2, OUTPUT);
+
+  // Setting Wifi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
+
+  // Waiting For connection
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
@@ -74,22 +86,22 @@ void setup()
 
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-  /* Assign the api key (required) */
+  // Api key (Important to link with database)
   config.api_key = API_KEY;
 
-  /* Assign the user sign in credentials */
+  // Sign in credentials
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
 
-  /* Assign the RTDB URL (required) */
+  // The Database URL */
   config.database_url = DATABASE_URL;
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // addons/TokenHelper.h
 
   Firebase.begin(&config, &auth);
 
-  // Comment or pass false value when WiFi reconnection will control by your code or third party library
+  // Comment or pass false value when WiFi reconnection will control manually or third party library
   Firebase.reconnectWiFi(true);
 
   Firebase.setDoubleDigits(5);
@@ -97,208 +109,30 @@ void setup()
 
 void loop()
 {
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 100 || sendDataPrevMillis == 0))
+  // Getting Firebase Ready and delay
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 500 || sendDataPrevMillis == 0))
   {
-    sendDataPrevMillis = millis();
-    // if (Firebase.RTDB.setInt(&fbdo, "Lock/int", 1))
-    // {
-    //   // Serial.println("PASSED");
-    //   // Serial.println("PATH: " + fbdo.dataPath());
-    //   // Serial.println("TYPE: " + fbdo.dataType());
-    //   Serial.print("True ");
-    //   Serial.println(read_data); // print the data received from the Firebase database
-    //   digitalWrite(2, HIGH);
-    // }
-    // else
-    // {
-    //   Serial.println("FAILED");
-    //   Serial.println("REASON: " + fbdo.errorReason());
-    // }
+    sendDataPrevMillis = millis(); // For delay
 
-    if (Firebase.RTDB.getInt(&fbdo, "/test/int"))
-
+    // Write Data
+    if (Firebase.RTDB.setInt(&fbdo, "test/int", n))
     {
-
-      if (fbdo.dataType() == "int")
-      {
-
-        read_data = fbdo.intData();
-        if (read_data == 0)
-        {
-          if (Firebase.RTDB.setInt(&fbdo, "Lock/int", 0))
-          {
-            // Serial.println("PASSED");
-            // Serial.println("PATH: " + fbdo.dataPath());
-            // Serial.println("TYPE: " + fbdo.dataType());
-            Serial.print("False ");
-            Serial.println(read_data); // print the data received from the Firebase database
-            digitalWrite(2, LOW);
-          }
-          else
-          {
-            Serial.println("FAILED");
-            Serial.println("REASON: " + fbdo.errorReason());
-          }
-        }
-        else if (read_data == 1)
-        {
-          if (Firebase.RTDB.setInt(&fbdo, "Lock/int", 1))
-          {
-            // Serial.println("PASSED");
-            // Serial.println("PATH: " + fbdo.dataPath());
-            // Serial.println("TYPE: " + fbdo.dataType());
-            Serial.print("TRUE ");
-            Serial.println(read_data); // print the data received from the Firebase database
-            digitalWrite(2, HIGH);
-          }
-          else
-          {
-            Serial.println("FAILED");
-            Serial.println("REASON: " + fbdo.errorReason());
-          }
-        }
-      }
+      Serial.println("PASSED");
+      n++;
+    }
+    else
+    {
+      Serial.println("FAILED -> REASON: " + fbdo.errorReason());
     }
 
-    // if (Firebase.RTDB.getInt(&fbdo, "/user/start"))
-
-    // {
-
-    //   if (fbdo.dataType() == "int")
-    //   {
-
-    //     read_data = fbdo.intData();
-    //     if (read_data == 0)
-    //     {
-    //       Serial.print("FALSE");
-    //       Serial.println(read_data); // print the data received from the Firebase database
-    //       digitalWrite(2, LOW);
-    //     }
-    //     else
-    //     {
-    //       Serial.print("TRUE");
-    //       digitalWrite(2, HIGH);
-    //     }
-    //   }
-    // }
-
-    // else
-
-    // {
-
-    //   Serial.println(fbdo.errorReason()); // print he error (if any)
-    // }
+    // Read Data
+    if (Firebase.RTDB.getInt(&fbdo, "/test/int"))
+    {
+      read_data = fbdo.intData();
+      if (read_data == 5)
+      {
+        Serial.println("Data is: " + read_data);
+      }
+    }
   }
-
-  // Firebase.ready() should be called repeatedly to handle authentication tasks.
-  // millis() -> Returns the number of milliseconds passed since the Arduino board began running the current program. This number will overflow (go back to zero), after approximately 50 days.
-  // if (Firebase.ready() && (millis() - sendDataPrevMillis > 100 || sendDataPrevMillis == 0))
-  // {
-  //   sendDataPrevMillis = millis();
-
-  //   LockSignal = digitalRead(Lock);
-
-  //   EngineSignal = digitalRead(engine);
-
-  //   if (LockSignal == LOW)  // If car is locked
-  //   {
-  //     //LockCount = 0;
-  //     Serial.println("Car Locked");
-  //     DoorSignal = digitalRead(Door);
-  //     if (Firebase.RTDB.setInt(&fbdo, "Lock/int", LockSignal))
-  //     {
-  //       Serial.println("PASSED");
-  //       Serial.println("PATH: " + fbdo.dataPath());
-  //       Serial.println("TYPE: " + fbdo.dataType());
-  //     }
-  //     else
-  //     {
-  //       Serial.println("FAILED");
-  //       Serial.println("REASON: " + fbdo.errorReason());
-  //     }
-
-  //     if (DoorSignal == HIGH)
-  //     {
-  //       doorCount = 1;
-  //       Serial.println("Illegal Door Opening");
-  //       digitalWrite(Relay, HIGH);
-  //       if (Firebase.RTDB.setInt(&fbdo, "Door/int", doorCount))
-  //       {
-  //         Serial.println("PASSED");
-  //         Serial.println("PATH: " + fbdo.dataPath());
-  //         Serial.println("TYPE: " + fbdo.dataType());
-  //       }
-  //       else
-  //       {
-  //         Serial.println("FAILED");
-  //         Serial.println("REASON: " + fbdo.errorReason());
-  //       }
-  //     }
-  //     else
-  //     {
-  //       doorCount = 0;
-  //       Serial.println("Door Closed");
-  //       if (Firebase.RTDB.setInt(&fbdo, "Door/int", doorCount))
-  //       {
-  //         Serial.println("PASSED");
-  //         Serial.println("PATH: " + fbdo.dataPath());
-  //         Serial.println("TYPE: " + fbdo.dataType());
-  //       }
-  //       else
-  //       {
-  //         Serial.println("FAILED");
-  //         Serial.println("REASON: " + fbdo.errorReason());
-  //       }
-  //       if (EngineSignal == HIGH)
-  //       {
-  //         engineCount = 1;
-  //         Serial.println("Illegal Engine On");
-  //         digitalWrite(Relay, HIGH);
-  //         // Firebase
-  //         if (Firebase.RTDB.setInt(&fbdo, "EngineStarts/int", engineCount))
-  //         {
-  //           Serial.println("PASSED");
-  //           Serial.println("PATH: " + fbdo.dataPath());
-  //           Serial.println("TYPE: " + fbdo.dataType());
-  //         }
-  //         else
-  //         {
-  //           Serial.println("FAILED");
-  //           Serial.println("REASON: " + fbdo.errorReason());
-  //         }
-  //       }
-  //     }
-  //   }
-  //   else if (LockSignal == HIGH)
-  //   {
-  //     LockCount = 1;
-  //     Serial.println("Car Unlocked");
-  //     if (Firebase.RTDB.setInt(&fbdo, "Lock/int", LockCount))
-  //     {
-  //       Serial.println("PASSED");
-  //       Serial.println("PATH: " + fbdo.dataPath());
-  //       Serial.println("TYPE: " + fbdo.dataType());
-  //     }
-  //     else
-  //     {
-  //       Serial.println("FAILED");
-  //       Serial.println("REASON: " + fbdo.errorReason());
-  //     }
-  //   }
-  //   Serial.println();
-  //   delay(1000);
-
-  // if (Firebase.RTDB.setInt(&fbdo, "test/int", count))
-  // {
-  //   Serial.println("PASSED");
-  //   Serial.println("PATH: " + fbdo.dataPath());
-  //   Serial.println("TYPE: " + fbdo.dataType());
-  //   count++;
-  // }
-  // else
-  // {
-  //   Serial.println("FAILED");
-  //   Serial.println("REASON: " + fbdo.errorReason());
-  // }
-  //}
 }
